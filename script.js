d73d0bfe22ac97e32bf772cb43e8fbc9aa7d9447
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-09 22:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+function sortedArray (arr){
+	let pattern=/The/;
+	let ans=[];
+	arr.forEach((a,i,ray)=>{
+		ans.push(arr[i].replace(pattern,"").trim());
+	})
+	console.log(ans.sort);
+	
+}
+let arr=['The Virupaksha Temple' , 'Victoria Memorial'
+		 , 'Tajmahal'];
+
+sortedArray(arr);
 
